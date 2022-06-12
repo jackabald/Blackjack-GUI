de167c93cdcd2b538db2cf3bcbdf710e3bb5f950
JackArchibald <jwarchibald@wisc.edu>
2022-06-11 22:11:17 -0400

    GUI window created - No buttons or graphics

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,3 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Blackjack {
-    
+    public Blackjack() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(500, 500, 175, 500));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Blackjack");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args){
+        new Blackjack();
+    }
 }
