629a65a84ba75b35a1223ecfde997c9dec5e13ad
JackArchibald <jwarchibald@wisc.edu>
2022-06-15 23:16:51 -0400

    Refactoring in Blackjack class

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,15 +1,23 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Blackjack {
-    int hand = 0;
+    // value of hands
+    int usersHand = 0;
     int dealersHand = 0;
+    // frame and panel
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
+    // buttons
+    JButton hitButton = new JButton("Hit");
+    JButton standButton = new JButton("Stand");
+    // labels
+    JLabel usersHandText = new JLabel();
+    JLabel dealersHandText = new JLabel();
 
     public Blackjack() {
-        button();
         setup();
+        buttons();
+        textFields();
     }
     public void setup(){
         // Setting frame and panel
@@ -18,25 +26,29 @@ public class Blackjack {
         frame.setVisible(true);
         frame.add(panel);
         panel.setLayout(null);
-
     }
-    public void button(){
-        // Create Buttons
-        JButton hitButton = new JButton("Hit");
-        JButton standButton = new JButton("Stand");
-
+    public void buttons(){
         // Add Buttons to panel
         panel.add(hitButton);
         panel.add(standButton);
         hitButton.setBounds(250,400,80,25);
         standButton.setBounds(150,400,80,25);
-
+    }
+    public void textFields(){
         // Add usersHand text field
-        JLabel usersHand = new JLabel(Integer.toString(hand));
-        usersHand.setBounds(237,350,165,25);
-        panel.add(usersHand);
+        JLabel usersHandText = new JLabel(Integer.toString(usersHand));
+        usersHandText.setBounds(237,350,165,25);
+        panel.add(usersHandText);
+
+        // Add dealersHand text field
+        JLabel dealerLabel = new JLabel("DEALER");
+        dealerLabel.setBounds(215,70,165,25);
+        JLabel dealersHandText = new JLabel(Integer.toString(dealersHand));
+        dealersHandText.setBounds(237,100,165,25);
+        panel.add(dealerLabel);
+        panel.add(dealersHandText);
     }
     public static void main(String[] args){
-        new Blackjack();
+        Blackjack newGame = new Blackjack();
     }
 }
